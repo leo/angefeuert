76d963652c5762aad84f01f8839bc5b6a6bc3ece
Leo Lamprecht <mindrun@icloud.com>
2016-06-24 18:40:41 +0200

    jQuery global isn't defined

--- app/components/nav-toggler.js
+++ app/components/nav-toggler.js
@@ -6,11 +6,11 @@ export default Ember.Component.extend({
   attributeBindings: ['href'],
   href: '#',
   click (event) {
-    const button = $(event.target).closest('.toggle')
+    const button = Ember.$(event.target).closest('.toggle');
 
-    button.closest('.low').prev().slideToggle(300)
-    button.toggleClass('on')
+    button.closest('.low').prev().slideToggle(300);
+    button.toggleClass('on');
 
-    event.preventDefault()
+    event.preventDefault();
   }
 });
