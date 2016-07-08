365b368340e120103b070dcd5c877440ab1bbbe0
Leo Lamprecht <mindrun@icloud.com>
2016-07-08 10:52:24 +0200

    Import isotope

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,8 +4,10 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
-    
+
   });
+
+  app.import(app.bowerDirectory + '/isotope/dist/isotope.pkgd.js')
 
   // Use `app.import` to add additional libraries to the generated
   // output files.
