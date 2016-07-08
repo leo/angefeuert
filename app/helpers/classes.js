1b01c500e094e23332bcefee14e552c46953c4bb
Leo Lamprecht <mindrun@icloud.com>
2016-07-08 12:42:58 +0200

    Join categories for class

---
+++ app/helpers/classes.js
@@ -0,0 +1,7 @@
+import Ember from 'ember';
+
+export function classes (params) {
+  return params[0].join(' ');
+}
+
+export default Ember.Helper.helper(classes);
--- app/routes/partners.js
+++ app/routes/partners.js
@@ -1,12 +1,18 @@
 import Ember from 'ember';
+const { $ } = Ember;
 
 export default Ember.Route.extend({
   titleToken: 'Partner',
   actions: {
     afterRender () {
-      const iso = new Isotope('.companies', {
+      const iso = $('.companies').isotope({
         itemSelector: '.item',
         layoutMode: 'fitRows'
+      })
+
+      $('.categories a').click(event => {
+        alert('soos')
+        event.preventDefault()
       })
     }
   },
@@ -14,47 +20,94 @@ export default Ember.Route.extend({
     return [
       {
         name: 'wamsler',
-        url: 'http://wamsler.eu'
+        url: 'http://wamsler.eu',
+        categories: [
+          'herde'
+        ]
       },
       {
         name: 'lohberger',
-        url: 'http://www.lohberger.com'
+        url: 'http://www.lohberger.com',
+        categories: [
+          'herde'
+        ]
       },
       {
         name: 'greithwald',
-        url: 'http://www.greithwald.com'
+        url: 'http://www.greithwald.com',
+        categories: [
+          'herde'
+        ]
       },
       {
         name: 'hagos',
-        url: 'http://www.hagos.de'
+        url: 'http://www.hagos.de',
+        categories: [
+          'kaminöfen',
+          'speicheröfen',
+          'kachelöfen',
+          'heizkamine',
+          'herde'
+        ]
       },
       {
         name: 'termatech',
-        url: 'http://www.termatech.co.uk'
+        url: 'http://www.termatech.co.uk',
+        categories: [
+          'kaminöfen'
+        ]
       },
       {
         name: 'ganz',
-        url: 'http://www.ganz-baukeramik.ch'
+        url: 'http://www.ganz-baukeramik.ch',
+        categories: [
+          'kaminöfen'
+        ]
       },
       {
         name: 'sommerhuber',
-        url: 'http://www.sommerhuber.com'
+        url: 'http://www.sommerhuber.com',
+        categories: [
+          'speicheröfen',
+          'kachelöfen',
+          'heizkamine'
+        ]
       },
       {
         name: 'tulikivi',
-        url: 'http://www.tulikivi.com'
+        url: 'http://www.tulikivi.com',
+        categories: [
+          'kaminöfen',
+          'speicheröfen'
+        ]
       },
       {
         name: 'brunner',
-        url: 'https://www.brunner.de'
+        url: 'https://www.brunner.de',
+        categories: [
+          'kaminöfen',
+          'speicheröfen',
+          'kachelöfen',
+          'heizkamine',
+          'herde'
+        ]
       },
       {
         name: 'leda',
-        url: 'http://www.leda.de'
+        url: 'http://www.leda.de',
+        categories: [
+          'kachelöfen',
+          'kaminöfen',
+          'heizkamine'
+        ]
       },
       {
         name: 'spartherm',
-        url: 'http://www.spartherm.com/startseite/'
+        url: 'http://www.spartherm.com/startseite/',
+        categories: [
+          'heizkamine',
+          'speicherkamine'
+        ]
       }
     ]
   }
