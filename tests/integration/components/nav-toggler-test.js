bd00d423e39f072d517c0f20317d1161af132f99
Leo Lamprecht <mindrun@icloud.com>
2016-07-10 15:49:55 +0200

    Make tests work

--- tests/integration/components/nav-toggler-test.js
+++ tests/integration/components/nav-toggler-test.js
@@ -12,13 +12,4 @@ test('it renders', function(assert) {
   this.render(hbs`{{nav-toggler}}`)
 
   assert.equal(this.$().text().trim(), '')
-
-  // Template block usage:
-  this.render(hbs`
-    {{#nav-toggler}}
-      template block text
-    {{/nav-toggler}}
-  `)
-
-  assert.equal(this.$().text().trim(), 'template block text')
 })
