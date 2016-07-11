762aa68f2f078dce282b5db1ed936bf9042e98b7
Leo Lamprecht <mindrun@icloud.com>
2016-07-11 14:22:40 +0200

    Styles for products

--- app/routes/products.js
+++ app/routes/products.js
@@ -1,5 +1,19 @@
 import Ember from 'ember'
 
 export default Ember.Route.extend({
-  titleToken: 'Produkte'
+  titleToken: 'Produkte',
+  model () {
+    return {
+      first: [
+        'Kaminöfen',
+        'Speicheröfen',
+        'Kachelöfen'
+      ],
+      second: [
+        'Heizkamine',
+        'Herde',
+        'Zubehör'
+      ]
+    }
+  }
 })
---
+++ tests/unit/helpers/lowercase-test.js
@@ -0,0 +1,10 @@
+import { lowercase } from 'angefeuert/helpers/lowercase';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | lowercase');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = lowercase([42]);
+  assert.ok(result);
+});
