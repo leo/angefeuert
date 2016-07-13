2f551348e09c26ae9c7dfae5ff247ffcc2f18ce9
Leo Lamprecht <mindrun@icloud.com>
2016-07-13 11:32:15 +0200

    Use slick slider for products

---
+++ app/controllers/products.js
@@ -0,0 +1,10 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  breakpoints: [
+    {
+      breakpoint: 0,
+      settings: 'unslick'
+    }
+  ]
+});
--- app/routes/products.js
+++ app/routes/products.js
@@ -3,17 +3,13 @@ import Ember from 'ember'
 export default Ember.Route.extend({
   titleToken: 'Produkte',
   model () {
-    return {
-      first: [
-        'Kaminöfen',
-        'Speicheröfen',
-        'Kachelöfen'
-      ],
-      second: [
-        'Heizkamine',
-        'Herde',
-        'Zubehör'
-      ]
-    }
+    return [
+      'Kaminöfen',
+      'Speicheröfen',
+      'Kachelöfen',
+      'Heizkamine',
+      'Herde',
+      'Zubehör'
+    ]
   }
 })
