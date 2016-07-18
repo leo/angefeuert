f6b1b049aee5b71182b8fee4da6749b1e1ea6c77
Leo Lamprecht <mindrun@icloud.com>
2016-07-18 17:02:58 +0200

    Great images for kacheloefen

--- app/routes/products/detail.js
+++ app/routes/products/detail.js
@@ -20,7 +20,7 @@ export default Ember.Route.extend({
     const slides = {
       kaminöfen: 3,
       speicheröfen: 6,
-      kachelöfen: 5,
+      kachelöfen: 2,
       heizkamine: 4,
       herde: 5,
       zubehör: 4
